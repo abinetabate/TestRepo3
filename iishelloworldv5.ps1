334dd19227f8037a17ae8222d6df792ff207af4d
abinetabate <102200733+abinetabate@users.noreply.github.com>
2022-07-07 13:07:26 -0700

    Update iishelloworldv5.ps1

--- iishelloworldv5.ps1
+++ iishelloworldv5.ps1
@@ -1,12 +1,2 @@
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/abinetabate/TestRepo3/master/appsettings.Development.json' -OutFile 'C:\inetpub\wwwroot\appsettings.Development.json'
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/abinetabate/TestRepo3/master/appsettings.json' -OutFile 'C:\inetpub\wwwroot\appsettings.json'
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/abinetabate/TestRepo3/master/aspnetcoreapp.deps.json' -OutFile 'C:\inetpub\wwwroot\aspnetcoreapp.deps.json'
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/abinetabate/TestRepo3/master/aspnetcoreapp.dll' -OutFile 'C:\inetpub\wwwroot\aspnetcoreapp.dll'
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/abinetabate/TestRepo3/master/aspnetcoreapp.exe' -OutFile 'C:\inetpub\wwwroot\aspnetcoreapp.exe'
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/abinetabate/TestRepo3/master/aspnetcoreapp.pdb' -OutFile 'C:\inetpub\wwwroot\aspnetcoreapp.pdb'
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/abinetabate/TestRepo3/master/aspnetcoreapp.runtimeconfig.json' -OutFile 'C:\inetpub\wwwroot\aspnetcoreapp.runtimeconfig.json'
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/abinetabate/TestRepo3/web.config' -OutFile 'C:\inetpub\wwwroot\web.config'
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/abinetabate/TestRepo3/wwwroot/aspnetcoreapp.styles.css' -OutFile 'C:\inetpub\wwwroot\aspnetcoreapp.styles.css'
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/abinetabate/TestRepo3/wwwroot/favicon.ico' -OutFile 'C:\inetpub\wwwroot\favicon.ico'
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/abinetabate/TestRepo3/wwwroot/css/site.css' -OutFile 'C:\inetpub\wwwroot\css\site.css'
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/abinetabate/TestRepo3/wwwroot/js/site.js' -OutFile 'C:\inetpub\wwwroot\css\site.js'
+Add-WindowsFeature Web-Server
+Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Hello World from host $($env:computername) !"
