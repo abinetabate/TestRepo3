c7ad8b82141ed5117e0849c4f66c740d37e6cbf0
abinetabate <102200733+abinetabate@users.noreply.github.com>
2022-07-07 13:13:53 -0700

    Update iishelloworldv5.ps1

--- iishelloworldv5.ps1
+++ iishelloworldv5.ps1
@@ -1,2 +1,3 @@
 Add-WindowsFeature Web-Server
+Rename-Item -Path "C:\inetpub\wwwroot\old_d.htm" -NewName "Default.htm"
 Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Hello World from host $($env:computername) !"
