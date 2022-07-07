8d3ba4f8d6f726edf963c806a435641ebbc5e2b2
abinetabate <102200733+abinetabate@users.noreply.github.com>
2022-07-06 18:09:08 -0700

    Update iishelloworldv5.ps1

--- iishelloworldv5.ps1
+++ iishelloworldv5.ps1
@@ -1,5 +1,4 @@
-Rename-Item "C:\inetpub\wwwroot\Default.htm" -NewName "old_d.htm"
 Add-WindowsFeature Web-Server
+Rename-Item "C:\inetpub\wwwroot\Default.htm" -NewName "old_d.htm"
 winget install git
-cd C:\inetpub\wwwroot\
-git clone https://github.com/abinetabate/TestRepo3.git
+git clone https://github.com/abinetabate/TestRepo3.git C:\inetpub\wwwroot\
